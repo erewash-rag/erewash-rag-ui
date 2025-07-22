2b013e7836bb8034487c0f8a061979798b0bcd57
Matt Micklewright <matthewmicklewright@gmail.com>
2025-07-22 18:43:47 +0100

    Delete build dir

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 // API Configuration
 // For production: 'https://4uzc2o31g0.execute-api.eu-west-2.amazonaws.com/prod'
 // For development: Using proxy to 'http://localhost:8080'
-export const API_BASE_URL = process.env.NODE_ENV === 'development' ? '' : 'https://4uzc2o31g0.execute-api.eu-west-2.amazonaws.com/prod';
+export const API_BASE_URL = process.env.NODE_ENV === 'development' ? '' : 'https://8h75w9vech.execute-api.eu-west-2.amazonaws.com/prod';
 
 // API Endpoints
 export const ARTICLES_ENDPOINT = `${API_BASE_URL}/articles`; 
